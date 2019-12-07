6885aeb1df6ac53a9b20cb07f7884ebe45b7713b
Ram <Ram@Ram-PC>
2019-12-07 23:48:47 +0530

    check  the program

--- JavaProgram/src/com/java/java8stream/TestStream.java
+++ JavaProgram/src/com/java/java8stream/TestStream.java
@@ -4,7 +4,13 @@ public class TestStream {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+  System.out.println("hello the program the ");
+  System.out.println("hello the program the ");
+  System.out.println("hello the program the ");
+  System.out.println("hello the program the ");
+	
+	
+	
 	}
 
 }

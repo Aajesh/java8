bb76ba288fce242cd820f90e7c1f3fdfffd820af
Ram <Ram@Ram-PC>
2019-12-07 23:59:19 +0530

    ffzdsf

---
+++ JavaProgram/src/com/java/stringtest/Snippet.java
@@ -0,0 +1,10 @@
+package com.java.stringtest;
+
+public class Snippet {
+	public static void main(String[] args) {
+		System.out.println("hello t check the program tehjasd he program the ");
+			
+			
+	}
+}
+

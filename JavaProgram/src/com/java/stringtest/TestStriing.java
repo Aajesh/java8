75241ec80d09f48704e6f8e55173ce430528fe2d
Ram <Ram@Ram-PC>
2019-12-07 23:57:09 +0530

    the the update the program

---
+++ JavaProgram/src/com/java/stringtest/TestStriing.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.java.stringtest;
+
+/**
+ * @author Ram
+ *
+ */
+public class TestStriing {
+
+	/**
+	 * 
+	 */
+	public TestStriing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

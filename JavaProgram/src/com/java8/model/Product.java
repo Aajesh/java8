24599a1d8bf6e17228102f2f8ef75a32a0443d6b
Ram <Ram@Ram-PC>
2019-12-27 20:59:28 +0530

    Java Lambda Expression Example: Comparator

---
+++ JavaProgram/src/com/java8/model/Product.java
@@ -0,0 +1,12 @@
+package com.java8.model;
+public class Product{  
+	public int id;  
+   public String name;  
+   public float price;  
+    public Product(int id, String name, float price) {  
+        super();  
+        this.id = id;  
+        this.name = name;  
+        this.price = price;  
+    }  
+}

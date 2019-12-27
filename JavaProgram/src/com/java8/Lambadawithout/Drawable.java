5fce891c9b133b1ad17bd545af2f7c800c21b56b
Ram <Ram@Ram-PC>
2019-12-27 20:36:05 +0530

    without lambda expression

---
+++ JavaProgram/src/com/java8/Lambadawithout/Drawable.java
@@ -0,0 +1,4 @@
+package com.java8.Lambadawithout;
+interface Drawable{  
+    public void draw();  
+}  
---
+++ JavaProgram/src/com/java8/Lambadawithout/LambdaExpressionExample.java
@@ -0,0 +1,12 @@
+package com.java8.Lambadawithout;
+public class LambdaExpressionExample {  
+    public static void main(String[] args) {  
+        int width=10;  
+  
+        //without lambda, Drawable implementation using anonymous class  
+        Drawable d=new Drawable(){  
+            public void draw(){System.out.println("Drawing "+width);}  
+        };  
+        d.draw();  
+    }  
+}  

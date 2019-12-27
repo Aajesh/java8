44ee94f0b0275a2c842efdb1a99ff9398efc1a78
Ram <Ram@Ram-PC>
2019-12-27 21:12:13 +0530

    not

--- JavaProgram/src/com/java8/lambdainterface/Sayable.java
+++ JavaProgram/src/com/java8/lambdainterface/Sayable.java
@@ -2,6 +2,7 @@ package com.java8.lambdainterface;
 @FunctionalInterface
 public interface Sayable{  
     //public String say(); 
-    public String say(String str); 
+    public String say(String message); 
     
+
 }  

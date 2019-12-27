4023d695b3e39d53dbd9d6a045b5d6f910bf84fc
Ram <Ram@Ram-PC>
2019-12-27 20:50:49 +0530

    Java Lambda Expression Example: Foreach Loop

---
+++ JavaProgram/src/com/java8/Lambadawith/LambdaExpressionExample7.java
@@ -0,0 +1,16 @@
+package com.java8.Lambadawith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaExpressionExample7{  
+    public static void main(String[] args) {  
+          
+        List<String> list=new ArrayList<String>();  
+        list.add("ankit");  
+        list.add("mayank");  
+        list.add("irfan");  
+        list.add("jai");  
+        list.forEach(str->System.out.println(str));
+    }   
+}
